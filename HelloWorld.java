9669489914d52c72954afd8fcb1ea12794a8c9da
Kumarpallav007 <46589811+Kumarpallav007@users.noreply.github.com>
2019-01-19 17:40:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
       System.out.println("Hello Bhadru kaisa hai, Our leaders");
-      System.out.println("Hello Bhadru kaisa hai, Our bkwass");
-      // Prints "Hello, I am here in the terminal window.
+      
    }
 }
